8614fc12660c821f0bb1819da57a0748b39d245c
Abreto Fu <public@abreto.email>
2020-10-07 20:20:32 +0800

    move .vimrc to vimrc

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@
 set -x
 
 ### Copy .vimrc to HOME folder ###
-ln -s ~/.vim/.vimrc ~/.vimrc
+# ln -s ~/.vim/.vimrc ~/.vimrc
 
 ### Install NERDTree ###
 vim -u NONE -c "helptags ~/.vim/pack/vendor/start/nerdtree/doc" -c q
